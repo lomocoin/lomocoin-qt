f3186e2be5803db706e5a15777bb34b18faa6d52
zihao <yaizhao@yaozihao.cn>
2017-02-14 11:17:25 +0800

    fix add .sh

---
+++ contrib/gistfile1.sh
@@ -0,0 +1,23 @@
+BIN_FILE=lomocoin-Qt.app/Contents/MacOS/lomocoin-Qt
+for P in `otool -L $BIN_FILE | awk '{print $1}'` 
+do 
+    if [[ "$P" == *//* ]] 
+    then 
+        PSLASH=$(echo $P | sed 's,//,/,g')
+        install_name_tool -change $P $PSLASH $BIN_FILE
+    fi 
+done 
+
+QTDIR=$1
+
+for F in `find $QTDIR/lib $QTDIR/plugins $QTDIR/qml  -perm 755 -type f` 
+do 
+    for P in `otool -L $F | awk '{print $1}'`
+	do   
+	    if [[ "$P" == *//* ]] 
+	    then 
+	        PSLASH=$(echo $P | sed 's,//,/,g')
+	        install_name_tool -change $P $PSLASH $F
+	    fi 
+	 done
+done
