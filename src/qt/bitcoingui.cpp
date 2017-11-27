ef6a555ea014e811690f7e4fbae12c64c3b811c3
supaiku <supaiku@protonmail.com>
2017-11-27 19:58:01 +0100

    Use QStandardPaths

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -820,7 +820,7 @@ void BitcoinGUI::encryptWallet(bool status)
 
 void BitcoinGUI::backupWallet()
 {
-    QString saveDir = QDesktopServices::storageLocation(QDesktopServices::DocumentsLocation);
+    QString saveDir = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation);
     QString filename = QFileDialog::getSaveFileName(this, tr("Backup Wallet"), saveDir, tr("Wallet Data (*.dat)"));
     if(!filename.isEmpty()) {
         if(!walletModel->backupWallet(filename)) {
