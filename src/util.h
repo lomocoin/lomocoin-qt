6fd892e59d55b7bac46de285de7733d5e59d928b
supaiku <supaiku@protonmail.com>
2017-11-27 19:35:45 +0100

    Fix OpenSSL 1.1.0 errors based on https://github.com/bitshares/bitshares-fc/commit/fee06a4#diff-6d61b48a989d754bfd35b29f526d5808

--- src/util.h
+++ src/util.h
@@ -343,7 +343,7 @@ typedef boost::interprocess::interprocess_semaphore CSemaphore;
 
 inline std::string i64tostr(int64 n)
 {
-    return strprintf("%"PRI64d, n);
+    return strprintf("%" PRI64d, n);
 }
 
 inline std::string itostr(int n)
